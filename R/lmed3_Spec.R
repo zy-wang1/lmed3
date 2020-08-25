4e2ce70f1559d46630ec5ec46b5f2a97fc31afdc
zy-wang1 <zy.wang1@outlook.com>
2020-08-24 23:28:40 -0400

    importing tmle3 and sl3

---
+++ R/lmed3_Spec.R
@@ -0,0 +1,56 @@
+#' Defines a TML Estimator (except for the data)
+#'
+#' Current limitations: pretty much tailored to \code{Param_TSM}
+#'
+#' @importFrom R6 R6Class
+#'
+#' @export
+#
+lmed3_Spec <- R6Class(
+  classname = "lmed3_Spec",
+  portable = TRUE,
+  class = TRUE,
+  public = list(
+    initialize = function(likelihood_override = NULL,
+                          variable_types = NULL, ...) {
+      private$.options <- list(
+        likelihood_override = likelihood_override,
+        variable_types = NULL, ...
+      )
+    },
+    make_tmle_task = function(data, node_list, ...) {
+      variable_types <- self$options$variable_types
+      tmle_task <- point_tx_task(data, node_list, variable_types)
+      return(tmle_task)
+    },
+    make_initial_likelihood = function(tmle_task, learner_list = NULL) {
+      # produce trained likelihood when likelihood_def provided
+      
+      if (!is.null(self$options$likelihood_override)) {
+        likelihood <- self$options$likelihood_override$train(tmle_task)
+      } else {
+        likelihood <- point_tx_likelihood(tmle_task, learner_list)
+      }
+      
+      return(likelihood)
+    },
+    make_updater = function(...) {
+      updater <- tmle3_Update$new(...)
+    },
+    make_targeted_likelihood = function(likelihood, updater) {
+      targeted_likelihood <- Targeted_Likelihood$new(likelihood, updater)
+      return(targeted_likelihood)
+    },
+    make_params = function(tmle_task, targeted_likelihood) {
+      stop("this is a base class, try tsm_Spec_TSM_all")
+    }
+  ),
+  active = list(
+    options = function() {
+      return(private$.options)
+    }
+  ),
+  private = list(
+    .options = NULL
+  )
+)
--- code/try_lmed3_202008.R
+++ code/try_lmed3_202008.R
@@ -1,5 +1,36 @@
 library(data.table)
 library(sl3)
 library(tlverse)
-library(dplyr)
+library(R6)  # R6Class
+library(dplyr)  # %>% 
+
+
+
+home <- getwd()  # specify it if needed
+source(file.path(home, "code", "basic_functions-202008.R"))
+source(file.path(home, "code", "generate_Zheng_data-202008.R"))
+source("./code/try_lmed3_to_import_202008.R")
+
+sample_size <- 100
+data_sim <- generate_Zheng_data(B = sample_size, tau = 2)
+data_wide <- data_sim %>% data.frame
+
+# all the imported class
+{
+  
+}
+
+# ARZLY model
+node_list <- list(L_0 = c("L1_0", "L2_0"), 
+                  A_1 = "A_1",
+                  R_1 = "R_1",
+                  Z_1 = "Z_1", 
+                  L_1 = "L1_1", 
+                  Y_1 = "Y_1", 
+                  A_2 = "A_2", 
+                  R_2 = "R_2", 
+                  Z_2 = "Z_2", 
+                  L_2 = "L1_2", 
+                  Y_2 = "Y_2" 
+                  )
 
