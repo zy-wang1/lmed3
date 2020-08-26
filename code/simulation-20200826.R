d04edc55557497b8add59e16c6c7501172d076b2
zy-wang1 <zy.wang1@outlook.com>
2020-08-26 17:07:40 -0400

    LY misspec

--- code/simulation-20200826.R
+++ code/simulation-20200826.R
@@ -192,3 +192,113 @@ for (sample_size in c(50
                                                                                                                                                                        file = paste0("./temp/", sample_size, "_onestep.tex")
   )
 }
+
+
+
+
+
+# LY model misspec
+
+node_list <- list(L_0 = c("L1_0", "L2_0"), 
+                  A_1 = "A_1",
+                  R_1 = "R_1",
+                  Z_1 = "Z_1", 
+                  L_1 = "L1_1", 
+                  Y_1 = "Y_1", 
+                  A_2 = "A_2", 
+                  R_2 = "R_2", 
+                  Z_2 = "Z_2", 
+                  L_2 = "L1_2", 
+                  Y_2 = "Y_2" 
+)
+
+data_truth <- generate_Zheng_data(B = 100000, tau = 2, seed = 202008, setAM = c(1, 0), if_LY_misspec = T)
+truth <- data_truth[[3]]$Y %>% mean
+
+n_sim <- 200
+# sample_size <- 50
+for (sample_size in c(
+  # 50
+                      # , 
+  100
+  , 400
+)) {
+  {
+    start.time <- Sys.time()
+    
+    # sample_size <- 100
+    RNGkind("L'Ecuyer-CMRG")
+    set.seed(123)
+    
+    results <- mclapply(1:n_sim, function(s) {
+      data_sim <- generate_Zheng_data(B = sample_size, tau = 2, if_LY_misspec = T)
+      temp_seq_reg <- est_seq_reg(data_sim = data_sim)
+      temp_density_sub <- est_density_sub(data_sim = data_sim)
+      temp_density_tmle <- est_density_tmle_1(data_sim = data_sim)
+      temp_density_onestep <- est_density_tmle_2(data_sim = data_sim)
+      
+      {
+        data_wide <- data.frame(data_sim)
+        middle_spec <- lmed_middle(
+          treatment_level = 1,
+          control_level = 0
+        )
+        tmle_task <- middle_spec$make_tmle_task(data_wide, node_list)
+        
+        # choose base learners
+        lrnr_mean <- make_learner(Lrnr_mean)
+        lrnr_glm <- make_learner(Lrnr_glm)
+        lrnr_glm_fast <- Lrnr_glm_fast$new(outcome_type = "binomial")
+        learner_list <- lapply(1:length(tmle_task$npsem), function(s) Lrnr_sl$new(
+          learners = list(
+            lrnr_mean,
+            # lrnr_glm, 
+            lrnr_glm_fast)
+        ))
+        # learner_list <- lapply(1:length(tmle_task$npsem), function(s) lrnr_glm_fast)  # simplest learner
+        names(learner_list) <- names(tmle_task$npsem)  # the first will be ignored; empirical dist. will be used for covariates
+        
+        initial_likelihood <- middle_spec$make_initial_likelihood(
+          tmle_task,
+          learner_list
+        )
+        
+        tmle_params <- middle_spec$make_params(tmle_task, initial_likelihood)[[1]]
+        temp_lmed3_nontargeting <- tmle_params$estimates(tmle_task)$psi
+      }
+      
+      return(c(temp_seq_reg, temp_density_sub, temp_density_tmle, temp_density_onestep, temp_lmed3_nontargeting
+      ))
+    }, mc.cores = nCores)
+    results <- results %>% abind(along = 0)
+    
+    end.time <- Sys.time()
+    time.taken <- end.time - start.time
+  }
+  
+  time.taken
+  rowmax <- apply(results, 1, max)
+  rowmax_noNA <- apply(results, 1, function(x) max(x, na.rm = T))
+  ifNA <- is.na(rowmax)
+  ifLarge <- rowmax_noNA > 10
+  
+  
+  # results <- results[!(ifNA & ifLarge), ]
+  report <- data.frame(Bias = apply(results, 2, function(s) mean(s, na.rm = T)) - truth, 
+                       lapply(1:ncol(results), function(which_col) c(mean((results[, which_col] - truth)^2, na.rm = T), 
+                                                                     sd(results[, which_col], na.rm = T))) %>% abind(along = 0)
+  )
+  names(report)[2:3] <- c("MSE", "SD")
+  rownames(report) <- c("Non-targeted Sequential Regression", "Non-targeted Density", "First-step Logistic MLE, Density", 
+                        "One-step MLE, Density", 
+                        "lmed3, Non-targeted"
+  )
+  report <- report[, c(2, 1, 3)]
+  report
+  
+  
+  report %>% xtable(type = "latex", caption = paste0("Sample size ", sample_size, "; run time: ", round(time.taken, 2), " ", units(time.taken), 
+                                                     "; # of NA and Large:  ",   sum(ifNA), " and ", sum(ifLarge)), digits = 6) %>% print(caption.placement = "top",
+                                                                                                                                                                       file = paste0("./temp/", sample_size, "LY_misspec.tex")
+  )
+}
